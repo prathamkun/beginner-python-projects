7fe596d4ae7e129b0681c9c55b3fdf16e629a73e
pratham <216220075+prathamkun@users.noreply.github.com>
2026-01-01 16:39:40 +0530

    Update 6quizprogram.py

--- 6quizprogram.py
+++ 6quizprogram.py
@@ -1,1 +1,50 @@
+quiz = {
+    "question1": {
+        "question": "What is capital of France?",
+        "answer": "Paris"
+    },
+    "question2": {
+        "question": "What is the capital of Germany?",
+        "answer": "Berlin"
+    },
+    "question3": {
+        "question": "What is the capital of Italy?",
+        "answer": "Rome"
+    },
+    "question4": {
+        "question": "What is the capital of Spain?",
+        "answer": "Madrid"
+    },
+    "question5": {
+        "question": "What is the capital of Portugal?",
+        "answer": "Lisbon"
+    },
+    "question6": {
+        "question": "What is the capital of Switzerland?",
+        "answer": "Bern"
+    },
+    "question7": {
+        "question": "What is the capital of Austria?",
+        "answer": "Vienna"
+    },
+}
+
+score = 0
+
+for key, value in quiz.items():
+    print(value['question'])
+    answer = input("Answer? ")
+
+    if answer.lower() == value['answer'].lower():
+        print("Correct")
+        score += 1
+    else:
+        print("Wrong!")
+        print("The answer is:", value['answer'])
+
+    print("Your score is:", score)
+    print()
+
+print("You got", score, "out of 7 questions correctly")
+print("Your percentage is", int(score / 7 * 100), "%")
 
